21c2c30c8bfb728d12fbd958b0cc78e3be512b52
liuchenwangmegt@163.com <19920229>
2018-10-25 14:52:21 +0800

    microservice

---
+++ microservicecloud/microservicecloud-provider-dept-8002/src/main/java/com/lingjoin/springcloud/ProviderApplication8002_App.java
@@ -0,0 +1,12 @@
+package com.lingjoin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderApplication8002_App {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication8002_App.class,args);
+    }
+
+}
